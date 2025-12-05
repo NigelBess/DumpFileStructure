09060f563e32a2e03a64d860104ac818a5717b77
Nigel-Bess <nigel.bess@fulfil.com>
2025-12-05 14:04:03 -0800

    wip

---
+++ DumpStructure/Extensions.cs
@@ -0,0 +1,20 @@
+﻿namespace DumpStructure;
+
+public static class Extensions
+{
+    public static string ToBytesString(this long value)
+    {
+        const long factor = 1024;
+        const long kb = factor;
+        const long mb = kb * factor;
+        const long gb = mb * factor;
+
+        if (value >= gb) return $"{value / gb}GB";
+        if (value >= mb) return $"{value / mb}MB";
+        if (value >= kb) return $"{value / kb}kB";
+        return $"{value}B";
+    }
+
+    public static string ToCountString(this int count, string singular, string? plural = null)
+        => $"{count} {(count == 1 ? singular : plural ?? singular + "s")}";
+}
--- DumpStructure/LeafFolder.cs
+++ DumpStructure/LeafFolder.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.IO;
+using System.Text;
+
 namespace DumpStructure;
 
 internal class LeafFolder : IFsObject
@@ -6,8 +9,37 @@ internal class LeafFolder : IFsObject
     public required string Name { get; init; }
     public required long SizeBytes { get; init; }
     public required string ContentsSummary { get; init; }
-    public string Render()
+    public LeafFolder(DirectoryInfo dir)
     {
-        throw new NotImplementedException();
+        Name = dir.Name;
+
+        long size = 0;
+        var files = 0;
+        var dirs = 0;
+
+        foreach (var _ in dir.EnumerateDirectories("*", SearchOption.AllDirectories)) dirs++;
+
+        foreach (var file in dir.EnumerateFiles("*", SearchOption.AllDirectories))
+        {
+            files++;
+            size += file.Length;
+        }
+
+        SizeBytes = size;
+        ContentsSummary = GenerateContentsSummary(files, dirs);
     }
+
+    private string GenerateContentsSummary(int files, int directories)
+    {
+        var sb = new StringBuilder();
+        if (files >= 0)
+        {
+            sb.Append(files.ToCountString("file"));
+            if (directories > 0) sb.Append(", ");
+        }
+        if (directories > 0) sb.Append(directories.ToCountString("directory", "directories"));
+        return sb.ToString();
+    }
+
+    public string Render() => $"{Name} - {SizeBytes.ToBytesString()} - {ContentsSummary}";
 }
