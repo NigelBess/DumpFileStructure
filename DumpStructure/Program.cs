77b4598da26af6d93fcc4cd0f9d6eac50c973445
Nigel-Bess <nigel.bess@fulfil.com>
2025-12-05 15:12:06 -0800

    fix exe

--- DumpStructure/Program.cs
+++ DumpStructure/Program.cs
@@ -1,19 +1,22 @@
-﻿using System.IO;
+﻿using DumpStructure;
+using System.IO;
 using System.Text;
 using System.Windows;
 
 static class Program
 {
+    const int defaultDepth = 3;
+
     [STAThread]
     static void Main(string[] args)
     {
         Console.OutputEncoding = Encoding.UTF8;
 
         var currentDirectory = Directory.GetCurrentDirectory();
-        var depth = args.Length > 0 && int.TryParse(args[0], out var d) ? d : 3;
-        var fileStructure = DirectoryStructureWriter.StructureString(currentDirectory, depth, true);
-
-        Console.Write(fileStructure);
-        Clipboard.SetText(fileStructure);
+        var depth = args.Length > 0 && int.TryParse(args[0], out var d) ? d : defaultDepth;
+        var folder = new Folder(new(currentDirectory), depth);
+        var text = folder.Render();
+        Console.WriteLine(text);
+        Clipboard.SetText(text);
     }
 }
